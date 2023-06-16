e76b5171b7d9b9ffc7c79b81b2e1a435e4a614c1
DonBet0n <mr.kostushevich@gmail.com>
2023-06-16 22:22:38 +0300

    first commit

---
+++ pr4/main.js
@@ -0,0 +1,38 @@
+// Image switcher code
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'subscription_red.png') {
+    myImage.setAttribute ('src','subscription_black.jpg');
+  } else {
+    myImage.setAttribute ('src','subscription_red.png');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Subscription on my channel, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Subscription successful, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
